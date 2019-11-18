afb61a59af94de5085c384bad5d4dcc697c0fe2c
wqq <1271088132@qq.com>
2019-11-18 16:42:32 +0800

    '组件化开发laravel的wechat商城'

---
+++ src/Wap/Member/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/11/18
+ * Time: 16:36
+ */
+
+namespace ChameleonW\LaravelShop\Wap\Member;
+
+
+class Test{
+    public function index(){
+        return 'Import Success! This is Chameleon-w/laravel-shop package';
+    }
+}
